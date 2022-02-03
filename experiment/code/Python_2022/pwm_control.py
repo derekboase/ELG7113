daf2de6fd26a8dbe70f3c73b36a7e1c6c49047cf
derekboase <boaseengineering@gmail.com>
2022-02-03 15:47:13 -0500

    Switched from the raspberry pi pico to the RPi4 to allow of the computationally demanding calculations that we're ultimately going to need

---
+++ experiment/code/Python_2022/pwm_control.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+
+from time import sleep
+
+PWM_PIN = 19 # Can use 12, 13, 19, 24
+
+def setup():
+	global pwm
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(PWM_PIN, GPIO.OUT)
+	GPIO.output(PWM_PIN, GPIO.LOW)
+	pwm = GPIO.PWM(PWM_PIN, 1000)
+	pwm.start(0)
+
+def varry_duty():
+	while True:
+		for duty in range(0, 101, 1):
+			pwm.ChangeDutyCycle(duty)
+			sleep(0.01)
+		sleep(1)
+		for duty in range(100, -1, -1):
+			pwm.ChangeDutyCycle(duty)
+			sleep(0.01)
+		sleep(1)
+
+def kill_pwm():
+	pwm.stop()
+	GPIO.output(PWM_PIN, GPIO.LOW)
+	GPIO.cleanup()
+
+if __name__ == "__main__":
+	setup()
+	try:
+		varry_duty()
+	except KeyboardInterrupt:
+		kill_pwm()
+	
