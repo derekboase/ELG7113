a696a2a826a9d5e1df1fa3f17eacc91c42fb592d
derekboase <boaseengineering@gmail.com>
2022-02-03 19:23:13 -0500

    Got lidar threading

--- experiment/code/Python_2022/tof_pwm_thread.py
+++ experiment/code/Python_2022/tof_pwm_thread.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as GPIO
 import VL53L1X as VL
 
+import threading
 from time import sleep
+
+PWM_PIN = 19 # Can use 12, 13, 19, 24
 
 def setup():
 	global pwm, tof
 	tof = VL.VL53L1X(i2c_bus=1, i2c_address=0x29)
-	tof.open()
 	
 	# Setting up the motor control PWM on pin 19 (BCM)
 	GPIO.setmode(GPIO.BCM)
@@ -15,14 +17,26 @@ def setup():
 	pwm = GPIO.PWM(PWM_PIN, 1000)
 	pwm.start(0)
 
-def reading_tof():
-	
 
-if __name__ == "__main__":
-	count = 0
-	while count < 20:
-		tof.start_ranging(1)
+def reading_tof():
+	while True:
 		distance_in_mm = tof.get_distance()
 		print(distance_in_mm)
+
+
+def changing_pwm(new_val):
+	pwm.ChangeDutyCycle(new_val)
+	sleep(0.01)
+
+
+if __name__ == "__main__":
+	setup()	
+	tof.open()
+	tof.start_ranging(1)
+	tof_thread = threading.Thread(target=reading_tof, daemon=True)
+	tof_thread.start()
+	count = 0
+	while count <20:
+		changing_pwm(float(input('Enter new value of PWM: ')))
 		count += 1
 	tof.stop_ranging()
