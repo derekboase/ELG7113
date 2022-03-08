97ff498916e65d0e77095ffdda6bab196612bb02
Clive <cplan082@uottawa.ca>
2022-03-07 21:29:36 -0500

    Added sample loop in fan_charac_HW_driver.ino. fan does not stop once test starts. Does not seem to increase speed either

--- experiment/code/fan_charac/fan_charac_HW_driver/fan_charac_HW_driver.ino
+++ experiment/code/fan_charac/fan_charac_HW_driver/fan_charac_HW_driver.ino
@@ -1,7 +1,11 @@
 #include <Wire.h>
 #include <SparkFun_FS3000_Arduino_Library.h>
+
 const byte pin_fan = 3;
+const byte num_samples = 100;
+const int delay_samples = 500; // must be more than 125ms (fastest air flow measurments can go)
 volatile bool int_flag = false;
+float duty_percent;
 
 FS3000 fs;
 
@@ -30,11 +34,17 @@ ISR(INT0_vect){
 void loop()
 {
     if(int_flag){
+      
       for(int i=180; i<=255; i += 5){
         analogWrite(pin_fan, i);
-        Serial.println(map_float(i, 0, 255, 0, 100));
-        Serial.println(fs.readMetersPerSecond()); // note, this returns a float from 0-7.23
-        delay(2000); // note, repsone time on the sensor is 125ms        
+        duty_percent = map_float(i, 0, 255, 0, 100);
+        
+        for(int j=0; j<=num_samples; j++){
+          Serial.println(duty_percent);
+          Serial.println(fs.readMetersPerSecond()); // note, this returns a float from 0-7.23
+          delay(delay_samples); // note, repsone time on the sensor is 125ms  
+        }
+              
       }
       analogWrite(pin_fan, 0);
       int_flag = false; // resets int_flag      
